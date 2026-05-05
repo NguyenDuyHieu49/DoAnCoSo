6f45cc1d72513cb131996dbbe10e526d44ab4bd9
NguyenDuyHieu49 <nguyenduyhieuwork49@gmail.com>
2026-05-06 01:54:35 +0700

    update weather forecast Hanoi

---
+++ DuLich/Core/Explore/View/WeatherEffect.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherEffect.swift
+//  DuLich
+//
+//  Created by Macbook Pro on 6/5/26.
+//
+
+import SwiftUI
+
+struct WeatherEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherEffect()
+}
